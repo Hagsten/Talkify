ecde18c153031cfd7999bdabcd6d272dd2babe3c
Hagsten <andreas.hagsten@gmail.com>
2015-10-28 18:06:02 +0100

    Extracted timer

---
+++ src/talkify-timer.js
@@ -0,0 +1,24 @@
+﻿function Timer() {
+    var callback, timerId, start, remaining;
+
+    this.pause = function () {
+        window.clearTimeout(timerId);
+        remaining -= new Date() - start;
+    };
+
+    this.resume = function () {
+        start = new Date();
+        window.clearTimeout(timerId);
+        timerId = window.setTimeout(callback, remaining);
+    };
+
+    this.cancel = function () {
+        window.clearTimeout(timerId);
+    };
+
+    this.start = function (cb, delay) {
+        callback = cb;
+        remaining = delay;
+        timerId = window.setTimeout(callback, remaining);
+    };
+}
