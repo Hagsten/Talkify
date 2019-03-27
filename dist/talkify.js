da039c9e5f78052510ed1c0ba2299b169abb8568
Andreas Hagsten <andreas.hagsten@gmail.com>
2019-03-27 21:32:53 +0100

    FIxed promise undefined using requirejs

--- dist/talkify.js
+++ dist/talkify.js
@@ -1,5 +1,5 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-window.promise = require('./src/promise.js').promise;
+window.promise = require('./src/promise.js');
 var talkify = require('./src/talkify.js');
 var talkifyConfig = require('./src/talkify-config.js');
 var talkifyUtils = require('./src/talkify-utils.js');
