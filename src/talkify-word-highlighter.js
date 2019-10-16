392e5da200e1f5d5108713d8897fbf54db2af29f
Hagsten <andreas.hagsten@gmail.com>
2019-10-16 21:55:11 +0200

    Revert text highlighter

--- src/talkify-word-highlighter.js
+++ src/talkify-word-highlighter.js
@@ -3,7 +3,6 @@ talkify.wordHighlighter = function (correlationId) {
     var currentItem = null;
     var currentPositions = [];
     var currentPosition = -1;
-
     talkify.messageHub.subscribe("word-highlighter", correlationId + ".player.tts.seeked", setPosition);
     talkify.messageHub.subscribe("word-highlighter", [correlationId + ".player.tts.loading", correlationId + ".player.tts.disposed"], cancel);
     talkify.messageHub.subscribe("word-highlighter", correlationId + ".player.tts.play", function (message) {
@@ -78,185 +77,24 @@ talkify.wordHighlighter = function (correlationId) {
 
         return internalPos;
     }
-
     function highlight(item, word, charPosition) {
         resetCurrentItem();
 
         currentItem = item;
+        var text = item.element.innerText.trim();
 
-        //Same as in playlist. Utilmetod?
-        item.element.innerHTML = item.element.innerHTML.replace(/ +/g, " ").replace(/(\r\n|\n|\r)/gm, "").trim();
+        var sentence = findCurrentSentence(item, charPosition);
 
-        highlightCurrentSentence(item.element, charPosition);
-        highlightCurrentWord(word, charPosition, 0, item.element.childNodes);
-    }
-
-    function highlightCurrentSentence(element, charPosition, currentPosition) {
-        var index = 0;
-
-        var sentence = findSentence(element.childNodes, 0);
-
-        index += sentence.text.length;
-
-        if (charPosition <= index) {
-            wrapSentence(sentence);
-
-            return;
-        }
-
-        while (sentence.next.length) {
-            sentence = findSentence(sentence.next, charPosition)
-
-            index += sentence.text.length;
-
-            if (charPosition <= index) {
-                wrapSentence(sentence);
-
-                return;
-            }
-        }
-    }
-
-    function wrapSentence(sentence) {
-        var wrapper = document.createElement('span');
-        wrapper.className = "talkify-sentence-highlight";
-
-        sentence.nodes[0].parentElement.insertBefore(wrapper, sentence.nodes[0]);
-
-        for (var i = 0; i < sentence.nodes.length; i++) {
-            wrapper.appendChild(sentence.nodes[i]);
-        }
-
-    }
-
-    function findSentence(nodes, textIndex) {
-        var nodesInSentence = [];
-        var nodesRemaining = [];
-        var textIndex = textIndex || 0;
-        var text = "";
-        var index = 0;
-
-        for (var i = 0; i < nodes.length; i++) {
-            index = i;
-            var node = nodes[i];
-
-            nodesInSentence.push(node);
-
-            if (node.nodeType === 3) { //textcontent
-                var indexOfSentenceEnd =
-                    ((node.textContent.indexOf(".") + 1) ||
-                        (node.textContent.indexOf("?") + 1) ||
-                        (node.textContent.indexOf("!") + 1) || 
-                        (node.textContent.indexOf("。") + 1)) - 1;
-
-                if (indexOfSentenceEnd > -1) {
-                    var rightHandSide = node.splitText(indexOfSentenceEnd + 1);
-
-                    nodesRemaining.push(rightHandSide);
-
-                    text += node.textContent;
-
-                    break;
-
-                } else {
-                    text += node.textContent;
-
-                    textIndex += node.textContent.length;
-                }
-
-            } else {
-                var response = findSentence(node.childNodes, textIndex);
-
-                textIndex += response.textIndex;
-                text += response.text;
-            }
-        }
-
-        for (var i = index + 1; i < nodes.length; i++) {
-            if (nodesRemaining.indexOf(nodes[i]) > -1) {
-                continue;
-            }
-
-            nodesRemaining.push(nodes[i]);
-        }
-
-        return {
-            nodes: nodesInSentence,
-            next: nodesRemaining,
-            text: text,
-            textIndex: textIndex
-        };
-    }
-
-    function highlightCurrentWord(word, charPosition, textIndex, nodes, previousCharWasWhitespace) {
-        var lastCharIsWhitespace = false;
-
-        for (var i = 0; i < nodes.length; i++) {
-            var childNode = nodes[i];
-
-            var isTextNode = childNode.nodeType === 3;
-
-            if (isTextNode) {
-                if (previousCharWasWhitespace && childNode.textContent.trim() === "") {
-                    continue;
-                }
-
-                var leadingWhiteSpaces = previousCharWasWhitespace ? 0 : childNode.textContent.length - childNode.textContent.trimStart().length;
-
-                if (textIndex > 0) {
-                    textIndex += leadingWhiteSpaces;
-                }
-
-                lastCharIsWhitespace = childNode.textContent.trimEnd() !== childNode.textContent;
-
-                var isInsideTextNode = childNode.textContent.indexOf(word) > -1 &&
-                    charPosition >= textIndex &&
-                    charPosition < textIndex + childNode.textContent.trimStart().length;
-
-                if (isInsideTextNode) {
-                    var splitOffset = charPosition - textIndex;
-                    var rigthHandSide = childNode.splitText(splitOffset);
-
-                    var wrapper = document.createElement('span');
-                    wrapper.className = "talkify-word-highlight";
-
-                    if (rigthHandSide.textContent.length > word.length) {
-                        var firstOccurranceOfWord = rigthHandSide.textContent.indexOf(word);
-
-                        if (firstOccurranceOfWord === 0) {
-                            rigthHandSide.splitText(word.length);
-                        } else {
-                            rigthHandSide = rigthHandSide.splitText(firstOccurranceOfWord);
-
-                            rigthHandSide.splitText(word.length);
-                        }
-                    }
-
-                    rigthHandSide.parentElement.insertBefore(wrapper, rigthHandSide);
-                    wrapper.appendChild(rigthHandSide);
-
-                    return {
-                        found: true,
-                        textIndex: textIndex
-                    };
-                }
-
-                textIndex += childNode.textContent.length - leadingWhiteSpaces;
-            } else {
-                var response = highlightCurrentWord(word, charPosition, textIndex, childNode.childNodes, lastCharIsWhitespace || previousCharWasWhitespace);
-
-                if (response.found) {
-                    return response;
-                }
-
-                textIndex = response.textIndex;
-            }
-        }
-
-        return {
-            found: false,
-            textIndex: textIndex
-        };
+        item.element.innerHTML =
+            text.substring(0, sentence.start) +
+            '<span class="talkify-sentence-highlight">' +
+            text.substring(sentence.start, charPosition) +
+            '<span class="talkify-word-highlight">' +
+            text.substring(charPosition, charPosition + word.length) +
+            '</span>' +
+            text.substring(charPosition + word.length, sentence.end) +
+            '</span>' +
+            text.substring(sentence.end);
     }
 
     function cancel() {
@@ -281,6 +119,7 @@ talkify.wordHighlighter = function (correlationId) {
         } else {
             currentPositions = positions;
         }
+
 
         var i = startFrom || 0;
 
@@ -339,7 +178,7 @@ talkify.wordHighlighter = function (correlationId) {
 
     function findCurrentSentence(item, charPosition) {
         var text = item.element.innerText.trim();
-        var result = text.match(/[^\.!\?]+[\.!\?]+/g) || [];
+        var result = text.match(/[^\.!\?。]+[\.!\?。]+/g) || [];
 
         var charactersTraversed = 0;
         var sentenceStart = 0;
